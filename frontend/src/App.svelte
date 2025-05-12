be4c2ceb8508af893c9a87e275d8543e28085fe4
anorak-lightman <164287745+anorak-lightman@users.noreply.github.com>
2025-05-12 14:40:24 -0700

    Fixed so html loads properly

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -15,14 +15,10 @@
 </script>
 
 <main>
-  <head>
-        <meta charset="UTF-8">
-        <title>The New York Times</title>
-        <link rel = "stylesheet" href = "../static/style.css">
-
-    </head>
-
-    <body>
+    <meta charset="UTF-8">
+    <title>The New York Times</title>
+    <link rel = "stylesheet" href = "../static/style.css">
+    
         <header>
             <div>
                 <ul class = "locations">
@@ -59,26 +55,12 @@
         <footer>
             <br>
             <hr class="footer">
-            <a href="https://developer.nytimes.com"><img src="../static/images/poweredby_nytimes_200a.png" width = 200px></a>
+            <a href="https://developer.nytimes.com"><img src="./images/poweredby_nytimes_200a.png" width = 200px></a>
         </footer>
-        <script src = "../static/main.js"></script>
-    </body>
+        <!-- <script src = "../static/main.js"></script> -->
+    
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
 </style>
