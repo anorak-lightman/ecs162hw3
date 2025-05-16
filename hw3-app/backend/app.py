a2bd330328b3bbdbe5fd64d7e33c0fac516b9eec
anorak-lightman <164287745+anorak-lightman@users.noreply.github.com>
2025-05-16 11:02:05 -0700

    nothing works, I am sad

--- hw3-app/backend/app.py
+++ hw3-app/backend/app.py
@@ -4,6 +4,10 @@ from flask_cors import CORS
 from pymongo import MongoClient
 import requests
 import json
+from flask import Flask, redirect, url_for, session
+from authlib.integrations.flask_client import OAuth
+from authlib.common.security import generate_token
+import os
 
 sacramento_url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q=Sacramento fq=timesTag.subject:"Sacramento" AND timesTag.location:"California"&api-key='
 davis_url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?q="UC Davis"&api-key='
@@ -18,6 +22,26 @@ mongo = MongoClient(mongo_uri)
 
 app = Flask(__name__, static_folder=static_path, template_folder=template_path)
 CORS(app)
+app.secret_key = os.urandom(24)
+
+
+oauth = OAuth(app)
+
+nonce = generate_token()
+
+
+oauth.register(
+    name=os.getenv('OIDC_CLIENT_NAME'),
+    client_id=os.getenv('OIDC_CLIENT_ID'),
+    client_secret=os.getenv('OIDC_CLIENT_SECRET'),
+    #server_metadata_url='http://dex:5556/.well-known/openid-configuration',
+    authorization_endpoint="http://localhost:5556/auth",
+    token_endpoint="http://dex:5556/token",
+    jwks_uri="http://dex:5556/keys",
+    userinfo_endpoint="http://dex:5556/userinfo",
+    device_authorization_endpoint="http://dex:5556/device/code",
+    client_kwargs={'scope': 'openid email profile'}
+)
 
 @app.route('/api/key')
 def get_key():
@@ -39,6 +63,33 @@ def get_stories(city, pageNumber):
     print(response)
     stories = response["response"]["docs"]
     return jsonify({"stories": stories})
+
+@app.route('/')
+def home():
+    user = session.get('user')
+    if user:
+        return f"<h2>Logged in as {user['email']}</h2><a href='/logout'>Logout</a>"
+    return '<a href="/login">Login with Dex</a>'
+
+@app.route('/login')
+def login():
+    session['nonce'] = nonce
+    redirect_uri = 'http://localhost:8000/authorize'
+    return oauth.flask_app.authorize_redirect(redirect_uri, nonce=nonce)
+
+@app.route('/authorize')
+def authorize():
+    token = oauth.flask_app.authorize_access_token()
+    nonce = session.get('nonce')
+
+    user_info = oauth.flask_app.parse_id_token(token, nonce=nonce)  # or use .get('userinfo').json()
+    session['user'] = user_info
+    return redirect('/')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect('/')
 
 @app.route('/')
 @app.route('/<path:path>')
